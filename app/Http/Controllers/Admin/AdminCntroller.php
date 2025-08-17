d8cc1e5fdf8cd55033132b11a89eae28591a3741
unknown <ssaeedi886@gmail.com>
2025-08-17 17:32:41 +0430

    array hard wordk

--- app/Http/Controllers/Admin/AdminCntroller.php
+++ app/Http/Controllers/Admin/AdminCntroller.php
@@ -54,24 +54,24 @@ class AdminCntroller extends Controller
 
         $filters = $request->input('filters', []);
 
-        $availableOperatorsNumbers = [
-            'EQ' => '=',
-            'GT' => '>',
-            'GTE' => '>=',
-            'LT' => '<',
-            'LTE' => '=<',
-            'NOT' => '!=',
-        ];
-
-        $availableOperatorsStrings = [
+        $availableOperatorString = [
             'LIKE' => 'like',
             'NOT' => '!=',
             'EQ' => '='
         ];
 
-        $availableLogics = [
+        $availableOperatorNumber = [
+            'EQ' => '=',
+            'GT' => '>',
+            'GTE' => '>=',
+            'LT' => '<',
+            'LTE' => '=<',
+            'NOT' => '!='
+        ];
+
+        $availableLogical = [
             'AND' => 'and',
-            'OR' => 'or',
+            'OR' => 'or'
         ];
 
         $fieldTypes = [
@@ -79,37 +79,135 @@ class AdminCntroller extends Controller
             'first_name' => 'string',
             'last_name' => 'string',
             'mobile' => 'string',
-            'national_code' => 'string',
             'father_name' => 'string',
+            'national_code' => 'string'
+        ];
+
+
+        $preparedFilters = [
+            [
+                'name' => 'first_name',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'string',
+                'operators' => [
+                    'LIKE',
+                    'NOT',
+                    'EQ'
+                ]
+            ],
+            [
+                'name' => 'last_name',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'string',
+                'operators' => [
+                    'LIKE',
+                    'NOT',
+                    'EQ'
+                ],
+            ],
+            [
+                'name' => 'father_name',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'string',
+                'operators' => [
+                    'LIKE',
+                    'NOT',
+                    'EQ'
+                ],
+            ],
+            [
+                'name' => 'age',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'number',
+                'operators' => [
+                    'NOT',
+                    'EQ',
+                    'GT',
+                    'GTE',
+                    'LT',
+                    'LTE'
+                ]
+            ],
+            [
+                'name' => 'national_code',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'string',
+                'operators' => [
+                    'LIKE',
+                    'NOT',
+                    'EQ'
+                ]
+            ],
+            [
+                'name' => 'mobile',
+                'logics' => [
+                    'AND',
+                    'OR'
+                ],
+                'type' => 'string',
+                'operators' => [
+                    'LIKE',
+                    'NOT',
+                    'EQ'
+                ]
+            ]
         ];
 
         foreach ($filters as $field => $fieldFilters) {
-            foreach ($fieldFilters as $filter) {
-                $value = $filter['value'];
-                $logicalOperator = $availableLogics[$filter['logical'] ?? 'AND'];
-                $fieldType = $fieldTypes[$field] ?? 'string';
+//            dd($field);
 
-                if ($fieldType === 'string') {
-                    $operator = $availableOperatorsStrings[$filter['operator'] ?? 'EQ'];
-                } elseif ($fieldType === 'number') {
-                    $operator = $availableOperatorsNumbers[$filter['operator'] ?? 'EQ'];
+            foreach ($preparedFilters as $pFilter) {
+//                dd($pFilter);
+
+                if ($pFilter['name'] === $field) {
+//                    dd($pFilter);
+//                    dd($field);
+                    foreach ($fieldFilters as $filter) {
+
+//                        dd($filter);
+
+//                        مقداری ورودی کاربر در $preparedFilters وجود داره یا نه اگه وجود
+
+
+
+                        $value = $filter['value'] ?? null;
+
+//                        حالا برای چک کردن فیتر ها هست که اگر توی اگر توی آرایه هست یا نه
+// اگر که هست با توجه به کلید مقدارش را از آرایه بالایی ها در بیار
+
+                        $logicalOperator = $availableLogical[$filter['logical'] ?? 'AND'];
+                        $fieldType = $fieldTypes[$field] ?? 'string';
+
+                        if ($fieldType === 'string') {
+                            $operator = $availableOperatorString[$filter['operator'] ?? 'EQ'];
+                        } elseif ($fieldType === 'number') {
+                            $operator = $availableOperatorNumber[$filter['operator'] ?? 'EQ'];
+                        }
+                        if ($operator === 'like') {
+                            $value = "%$value%";
+                        }
+                        $userQuery->Where($field, $operator, $value, $logicalOperator);
+                    }
                 }
-                if ($operator === 'like') {
-                    $value = "%$value%";
-                }
-                $userQuery->where($field, $operator, $value, $logicalOperator);
+
             }
+
         }
-
-
-//        foreach ($filters as $field => $fieldFilters) {
-//            foreach ($fieldFilters as $filter) {
-//                $value = $filter['value'];
-//                $operator = $availableOperators[$filter['operator'] ?? 'EQ'];
-//                $logicalOperator = $availableLogics[$filter['logical'] ?? 'AND'];
-//                $userQuery->where($field, $operator, $value, $logicalOperator);
-//            }
-//        }
 
         $perPage = $request->input('per_page', 15);
         $user = $userQuery->paginate($perPage);
@@ -181,3 +279,171 @@ class AdminCntroller extends Controller
         return $this->createResponse(false, $message);
     }
 }
+
+//public function index(IndexUserRequest $request)
+//{
+//    $allowedCollections = ['mobile', 'national_code', 'id', 'first_name'];
+//
+//    $userQuery = User::query();
+//    $orderInput = $request->input('sort', self::DEFAULT_SORT);
+//    if (is_string($orderInput)) {
+//        $userQuery->orderBy($orderInput, self::DEFAULT_DIRECTION);
+//    } elseif (is_array($orderInput)) {
+//        $orderColumn = $allowedCollections[0] ?? self::DEFAULT_SORT;
+//        $orderDirection = $orderInput[1] ?? self::DEFAULT_DIRECTION;
+//        $orderDirection = in_array($orderDirection, ['asc', 'desc']) ?
+//            $orderDirection : self::DEFAULT_DIRECTION;
+//        $userQuery->orderBy($orderColumn, $orderDirection);
+//    }
+//
+//    $searchQuery = $request->input('search');
+//    if (!empty($searchQuery)) {
+//        $userQuery->where(function ($q) use ($searchQuery) {
+//            $q->where('first_name', 'LIKE', '%' . $searchQuery . '%')
+//                ->orWhere('last_name', 'LIKE', '%' . $searchQuery . '%')
+//                ->orWhere('mobile', 'LIKE', '%' . $searchQuery . '%')
+//                ->orWhere('national_code', 'LIKE', '%' . $searchQuery . '%');
+//        });
+//    }
+//
+//
+//    $filters = $request->input('filters', []);
+//
+//    $availableOperatorString = [
+//        'LIKE' => 'like',
+//        'NOT' => '!=',
+//        'EQ' => '='
+//    ];
+//
+//    $availableOperatorNumber = [
+//        'EQ' => '=',
+//        'GT' => '>',
+//        'GTE' => '>=',
+//        'LT' => '<',
+//        'LTE' => '=<',
+//        'NOT' => '!='
+//    ];
+//
+//    $availableLogical = [
+//        'AND' => 'and',
+//        'OR' => 'or'
+//    ];
+//
+//    $fieldTypes = [
+//        'age' => 'number',
+//        'first_name' => 'string',
+//        'last_name' => 'string',
+//        'mobile' => 'string',
+//        'father_name' => 'string',
+//        'national_code' => 'string'
+//    ];
+//
+//
+//    $preparedFilters = [
+//        [
+//            'name' => 'first_name',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'string',
+//            'operators' => [
+//                'LIKE',
+//                'NOT',
+//                'EQ'
+//            ]
+//        ],
+//        [
+//            'name' => 'last_name',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'string',
+//            'operators' => [
+//                'LIKE',
+//                'NOT',
+//                'EQ'
+//            ],
+//        ],
+//        [
+//            'name' => 'father_name',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'string',
+//            'operators' => [
+//                'LIKE',
+//                'NOT',
+//                'EQ'
+//            ],
+//        ],
+//        [
+//            'name' => 'age',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'number',
+//            'operators' => [
+//                'NOT',
+//                'EQ',
+//                'GT',
+//                'GTE',
+//                'LT',
+//                'LTE'
+//            ]
+//        ],
+//        [
+//            'name' => 'national_code',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'string',
+//            'operators' => [
+//                'LIKE',
+//                'NOT',
+//                'EQ'
+//            ]
+//        ],
+//        [
+//            'name' => 'mobile',
+//            'logics' => [
+//                'AND',
+//                'OR'
+//            ],
+//            'type' => 'string',
+//            'operators' => [
+//                'LIKE',
+//                'NOT',
+//                'EQ'
+//            ]
+//        ]
+//    ];
+//
+//    foreach ($filters as $field => $fieldFilters) {
+//
+//        foreach ($fieldFilters as $filter) {
+//
+//            $value = $filter['value'] ?? null;
+//            $logicalOperator = $availableLogical[$filter['logical'] ?? 'AND'];
+//            $fieldType = $fieldTypes[$field] ?? 'string';
+//
+//            if ($fieldType === 'string') {
+//                $operator = $availableOperatorString[$filter['operator'] ?? 'EQ'];
+//            } elseif ($fieldType === 'number') {
+//                $operator = $availableOperatorNumber[$filter['operator'] ?? 'EQ'];
+//            }
+//            if ($operator === 'like') {
+//                $value = "%$value%";
+//            }
+//            $userQuery->Where($field, $operator, $value, $logicalOperator);
+//        }
+//    }
+//
+//    $perPage = $request->input('per_page', 15);
+//    $user = $userQuery->paginate($perPage);
+//    return $this->createResponse(true, 'Users found successfully', $user);
+//}
